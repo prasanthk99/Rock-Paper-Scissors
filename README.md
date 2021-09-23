4ba5b2a0b00d41162ee820158caf2e8dc1469e3a
Prasanth K <88704195+prasanthk99@users.noreply.github.com>
2021-09-23 11:24:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Rock-Paper-Scissors
+# Frontend Mentor - Rock, Paper, Scissors solution
+
+This is a solution to the [Rock, Paper, Scissors challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+
+### Screenshot
+
+![](.images/screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+Using this project you can learn more about CSS Flexbox and JavaScript using jquery, it will make the website function. Mostly I have learned more things in JavScript.
+
+### Continued development
+
+Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+
+### Useful resources
+
+- [W3Schools](https://www.w3schools.com/css/) - This helped me to make style to my site.
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/CSS) - CSS.
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - JavaScript.
+
+## Author
+
+- Frontend Mentor - [@prasanthk99](https://www.frontendmentor.io/profile/prasanthk99)
+
+## Acknowledgments
+
+This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
---
+++ index.js
@@ -0,0 +1,150 @@
+
+var len = document.querySelectorAll(".img").length;
+var s; 
+var score = 12;
+
+// PickingUp-Choice
+
+for(var i =0;i<len;i++){
+    document.querySelectorAll(".imgs")[i].addEventListener("click",function(){
+        s = this.classList[0];
+        
+        var vis = document.querySelector(".main-content").style.visibility = "hidden";
+        
+        if(vis=="hidden"){
+            document.querySelector(".result-content").style.visibility = "visible"
+        }
+        switch(s){
+            case "paper":
+                document.querySelector(".user-img").src = "images/icon-paper.svg";
+                document.querySelector(".user-img").style.borderColor = "hsl(230, 89%, 65%)";
+            break;
+            case "scissors":
+                document.querySelector(".user-img").src = "images/icon-scissors.svg";
+                document.querySelector(".user-img").style.borderColor = "hsl(40, 84%, 53%)";
+
+
+            break;
+            case "rock":
+                document.querySelector(".user-img").src = "images/icon-rock.svg";
+                document.querySelector(".user-img").style.borderColor = "hsl(349, 70%, 56%)";
+
+            break;
+
+        }
+        setTimeout(result,500);
+
+        // document.querySelector(".res-img").src = "images/icon-"+ obj[random]+".svg";
+    });
+
+}
+
+// Result-Tab
+
+function result(){
+    var random = Math.floor(Math.random()*3);
+    var obj = ["paper","rock","scissors"];
+    var key = obj[random]
+    switch(key){
+        case "paper":
+            document.querySelector(".res-img").src = "images/icon-paper.svg";
+            document.querySelector(".res-img").style.borderColor = "hsl(230, 89%, 65%)";
+        break;
+        case "scissors":
+            document.querySelector(".res-img").src = "images/icon-scissors.svg";
+            document.querySelector(".res-img").style.borderColor = "hsl(40, 84%, 53%)";
+        break;
+        case "rock":
+            document.querySelector(".res-img").src = "images/icon-rock.svg";
+            document.querySelector(".res-img").style.borderColor = "hsl(349, 70%, 56%)";
+        break;
+
+    }
+    document.querySelector(".res-img").style.visibility = "visible";
+    
+    if(s==key){
+        document.querySelector(".result-txt p").textContent = "DRAW !";
+    }
+    else if(s == "paper" && key == "rock"){
+        document.querySelector(".result-txt p").textContent = "YOU WIN";
+    }
+    else if(s == "scissors" && key == "paper"){
+        document.querySelector(".result-txt p").textContent = "YOU WIN";
+    }
+    else if(s == "rock" && key == "scissors"){
+        document.querySelector(".result-txt p").textContent = "YOU WIN";
+    }
+    else{
+        document.querySelector(".result-txt p").textContent = "YOU LOSE";
+
+    }
+    document.querySelector(".result-txt").style.visibility = "visible"
+
+    scoreUpdate();
+}
+
+// Updating-Score
+
+function scoreUpdate(){
+    var res = document.querySelector(".result-txt p").textContent;
+    switch(res){
+        case "YOU WIN":
+            score = score + 1;
+        break;
+        case "YOU LOOSE":
+            if(score>0){
+                score = score - 1;
+            }    
+
+        break;
+        default:
+            score = score;
+    }
+    if(score > -1){
+        document.querySelector(".sc-number").textContent = score;
+    }
+}
+
+// PlayAgain-Btn-Function
+
+playagain = () =>{
+    var res = document.querySelector(".result-content").style.visibility = "hidden"
+    document.querySelector(".result-txt").style.visibility = "hidden"
+    
+    if(res=="hidden"){
+        document.querySelector(".main-content").style.visibility = "visible";
+        document.querySelector(".res-img").style.visibility = "hidden";
+    }
+    
+}
+
+document.querySelector(".play-again-btn").addEventListener("click",playagain);
+
+// Rules-PopUp-Function
+
+document.querySelector(".rules-btn").addEventListener("click",function(){
+
+    var ruleclick =  document.querySelector(".rule").style.display; 
+    if(ruleclick=="none"){
+        document.querySelector(".rule").style.display = "unset";
+        document.querySelector(".container").style.opacity = "0.2";
+
+    }
+    else if(ruleclick=="unset"){
+        document.querySelector(".rule").style.display = "none";
+        document.querySelector(".container").style.opacity = "1";
+        
+    }  
+      
+});
+
+
+
+// Close-Button
+
+function close(){
+    document.querySelector(".rule").style.display = "none";
+    document.querySelector(".container").style.opacity = "1";
+
+}
+document.querySelector(".close").addEventListener("click",close);
